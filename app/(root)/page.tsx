dadb5fa10204cd7ed2ec0d158c527d0968a85475
Sheriff Aderombi <ayaderombi@gmail.com>
2025-03-06 23:54:17 +0100

    added card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,42 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartUpCard from "@/components/StartUpCard";
 
 export default async function Home({searchParams}: {
   searchParams: Promise<{query ?: string}>
 }) {
 
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {_id: 1, name: 'Sheriff'},
+    _id: 1,
+    description: 'This is a description.',
+    image: 'https://unsplash.com/photos/a-woman-standing-in-front-of-a-mirror-brushing-her-teeth-vuwDHNmwvHQ',
+    category: 'Robots',
+    title: "We Robots"
+  }]
+
   return (
     <>
     <section className="pink_container">
       <h1 className="heading">Pitch your startup, <br />connect with entrepreneurs</h1>
       <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
       <SearchForm query={query}/>
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : 'All Startups'}
+      </p>
+      <ul className="mt-7 card_grid">
+        {posts?.length > 0 ? (
+          posts.map((post: StartUpCardType, index: number) => (
+            <StartUpCard key={post?._id} post={post}/>
+          ))) : (
+            <p className="no-results">No startups found</p>
+          )}
+      </ul>
     </section>
     </>
   );
---
+++ components/StartUpCard.tsx
@@ -0,0 +1,51 @@
+import { formatDate } from '@/lib/utils'
+import { EyeIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const StartUpCard = ({post} : {post: StartUpTypeCard}) => {
+    const {_createdAt, views, author: {_id: authorId, name}, title, category, _id, image, description} = post
+  return (
+    <li className='startup-card group'>
+        <div className='flex-between'>
+            <p className='startup-card-date'>
+                {formatDate(_createdAt)}
+            </p>
+            <div className="flex gap-1.5">
+                <EyeIcon className='size-6 text-primary'/>
+                <span className='text-16-medium'>{views}</span>
+            </div>
+        </div>
+
+        <div className='flex-between mt-5 gap-5'>
+            <div className='flex-1'>
+                <Link href={`/user/${authorId}`}>
+                    <p className='text-16-medium line-clamp-1'>{name}</p>
+                </Link>
+                <Link href={`/startup/${_id}`}>
+                    <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+                </Link>
+            </div>
+            <Link href={`/user/${authorId}`}>
+                <Image
+                    src="https://placehold.co/48x48"
+                    alt="placeholder"
+                    width={48}
+                    height={48}
+                    className="rounded-full"
+                 />
+            </Link>
+        </div>
+
+        <Link href={`/startup/${_id}`}>
+            <p className='startup-card_desc'>{description}</p>
+            <img src={image} alt="placeholder" className='startup-card_img'/>
+        </Link>
+        
+      
+    </li>
+  )
+}
+
+export default StartUpCard
